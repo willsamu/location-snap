57aa8c7ba6e2e604d3e1ff13cace804c5ae16fc7
willsamu <bok5@gmx.net>
2020-06-11 15:56:30 +0200

    Chore: Setup Config sample file

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,13 +1,12 @@
 const region = 'eu-central-1'
-const apiId = 'z291zzopbe'
-// const hostingUrl = 'https://d2ovsmn4ut11iq.cloudfront.net'
+const apiId = 'XXXXXXXXXX' // TODO: insert your API id
 const hostingUrl = 'http://localhost:3000'
 const stage = process.env.STAGE || 'dev'
-export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}` // TODO: add stage
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}`
 
 export const authConfig = {
-  domain: 'lambda-todo.eu.auth0.com', // Auth0 domain
-  clientId: 'yxC2Fn7F3qfKmH06E8c8Hjggbny70O9J', // Auth0 client id
+  domain: 'YOURDOMAIN.eu.auth0.com', // TODO: insert your Auth0 domain
+  clientId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', // TODO: insert  your Auth0 client id
   callbackUrl: `${hostingUrl}/callback`,
   websiteDomain: `${hostingUrl}`,
 }
