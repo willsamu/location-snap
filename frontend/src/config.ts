d094468ec063f4923ba829c32c09628ef5616a82
willsamu <bok5@gmx.net>
2020-06-11 04:28:58 +0200

    Chore: Deployment Urls

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,11 +1,12 @@
 const region = 'eu-central-1'
 const apiId = 'z291zzopbe'
+const hostingUrl = 'https://d2ovsmn4ut11iq.cloudfront.net'
 const stage = process.env.STAGE || 'dev'
-export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}`
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}` // TODO: add stage
 
 export const authConfig = {
   domain: 'lambda-todo.eu.auth0.com', // Auth0 domain
   clientId: 'yxC2Fn7F3qfKmH06E8c8Hjggbny70O9J', // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback',
-  websiteDomain: 'http://localhost:3000',
+  callbackUrl: `${hostingUrl}/callback`,
+  websiteDomain: `${hostingUrl}`,
 }
